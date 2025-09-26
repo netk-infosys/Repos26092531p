0c9abe38e7146a0806e88ca1037da8f09383a281
netk-infosys <netk.infosys@gmail.com>
2025-09-26 08:52:48 +0300

    First commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vid
+{
+    public class Class1
+    {
+        public int a;
+        public int b;
+        public int sum = 0;
+    }
+}
